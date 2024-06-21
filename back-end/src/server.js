844d39374b516c53833ab92504cfebc3b6164d05
joel8oudreau <joel.boudreau@gmail.com>
2024-06-21 12:22:35 +0000

    prep for deploy

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 import path from 'path';
 async function start() {
 const url = `mongodb+srv://fsv-server:$$$Joel123$$$@cluster0.krabfxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
- const client = new MongoClient(url);
+  const client = new MongoClient(url);
 
   await client.connect();
   const db = client.db('fsv-db');
@@ -11,8 +11,12 @@ const url = `mongodb+srv://fsv-server:$$$Joel123$$$@cluster0.krabfxn.mongodb.net
   const app = express();
   app.use(express.json());
 
-  app.use('/images',express.static(path.join(__dirname,'../assets')));
-  
+  app.use('/images', express.static(path.join(__dirname, '../assets')));
+
+  app.use(express.static(
+    path.resolve(__dirname,'../dist'),
+    {maxAge: '1y', etag:false},
+  ));
   app.get('/api/products', async (req, res) => {
     const products = await db.collection('products').find({}).toArray();
     res.send(products);
@@ -24,7 +28,7 @@ const url = `mongodb+srv://fsv-server:$$$Joel123$$$@cluster0.krabfxn.mongodb.net
 
   app.get('/api/users/:userId/cart', async (req, res) => {
     const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
@@ -37,13 +41,18 @@ const url = `mongodb+srv://fsv-server:$$$Joel123$$$@cluster0.krabfxn.mongodb.net
   app.post('/api/users/:userId/cart', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
-    console.log(userId + ' ' + productId);
+
+    const existingUser = await db.collection('users').findOne({id:userId});
+
+    if(!existingUser){
+      await db.collection('users').insertOne({id:userId,cartItems : []});
+    }
     await db.collection('users').updateOne({ id: userId }, {
       $addToSet: { cartItems: productId }
     });
 
     const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
   });
 
@@ -51,17 +60,23 @@ const url = `mongodb+srv://fsv-server:$$$Joel123$$$@cluster0.krabfxn.mongodb.net
     const userId = req.params.userId;
     const productId = req.params.productId;
 
-    await db.collection('users').updateOne({id : userId},{
-      $pull: {cartItems : productId},
-  });
+    await db.collection('users').updateOne({ id: userId }, {
+      $pull: { cartItems: productId },
+    });
+
     const user = await db.collection('users').findOne({ id: req.params.userId });
-    const populatedCart = await populateCartIds(user.cartItems);
+    const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
-   
   });
 
-  app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
+  app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () => {
+    console.log('Server is listening on port ' + port);
   });
 }
 
--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -1,56 +1,87 @@
 <template>
   <div v-if="product">
-    <div class="img-wrap">
-      <img :src="product.imageUrl" />
-    </div>
-    <div class="product-details">
-      <h1>{{ product.name }}</h1>
-      <h3 class="price">{{ product.price }}</h3>
-      <button @click="addToCart" class="add-to-cart" v-if="!itemIsInCart">Add to cart</button>
-      <button class="grey-button" v-if="itemIsInCart">Item is already in cart</button>
-    </div>
+      <div class="img-wrap">
+        <img :src="product.imageUrl" />
+      </div>
+      <div class="product-details">
+        <h1>{{ product.name }}</h1>
+        <h3 class="price">{{ product.price }}</h3>
+        <button @click="addToCart" class="add-to-cart" v-if="user && !itemIsInCart">Add to cart</button>
+        <button class="grey-button" v-if="user && itemIsInCart">Item is already in cart</button>
+        <button class="sign-in" @click="signIn" v-if="!user">Sign in to add to cart</button>
+      </div>
   </div>
-  <div v-if="!product">
+  <div v-else>
     <NotFoundPage />
   </div>
 </template>
 
 <script>
-
-import NotFoundPage from './NotFoundPage.vue';
+import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink } from 'firebase/auth';
 import axios from 'axios';
-  export default{
-    name : "ProductDetailPage",
-    data() {
-      return {
-        product: {},
-        cartItems : [],
-      }
-    },
-    computed : {
-      itemIsInCart() {
-          return this.cartItems.some(item => item.id === this.$route.params.productId);
-      }
+import NotFoundPage from './NotFoundPage.vue';
 
-    },
-    methods : {
-      async addToCart(){
-        await axios.post('/api/users/12345/cart',{id : this.$route.params.productId});
-      //  alert(this.$route.params.productId);//ici c'est good...on a le bon 
-        alert('Successfully added item to cart');
+export default {
+  name: "ProductDetailPage",
+  props: ['user'],
+  data() {
+    return {
+      product: {},
+      cartItems: [],
+    }
+  },
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some(item => item.id === this.$route.params.productId);
+    }
+  },
+  watch: {
+    async user(newUserValue) {
+      if (newUserValue) {
+        const cartResponse = await axios.get(`/api/users/${newUserValue.uid}/cart`);
+        const cartItems = cartResponse.data;
+        this.cartItems = cartItems;
       }
+    }
+  },
+  methods: {
+    async addToCart() {
+      await axios.post(`/api/users/${this.user.uid}/cart`, { id: this.$route.params.productId });
+      alert('Successfully added item to cart!');
     },
-    components : {
-      NotFoundPage,
-    },
-    async created(){
-      const response = await axios.get(`/api/products/${this.$route.params.productId}`);
-      const product = response.data;
-      this.product = product;
+    async signIn() {
+      const email = prompt('Please enter your email to sign in:');
+      const auth = getAuth();
+      const actionCodeSettings = {
+        url: `https://full-stack-site-joe.onrender.com/products/${this.$route.params.productId}`,
+        handleCodeInApp: true,
+      }
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      alert('A login link was sent to the email you provided');
+      window.localStorage.setItem('emailForSignIn', email);
+    }
+  },
+  components: {
+    NotFoundPage
+  },
+  async created() {
+    const auth = getAuth();
+    if (isSignInWithEmailLink(auth, window.location.href)) {
+      const email = window.localStorage.getItem('emailForSignIn');
+      await signInWithEmailLink(auth, email, window.location.href);
+      alert('Successfully signed in!');
+      window.localStorage.removeItem('emailForSignIn');
+    }
 
-      const cartResponse = await axios.get('/api/users/12345/cart');
+    const response = await axios.get(`/api/products/${this.$route.params.productId}`);
+    const product = response.data;
+    this.product = product;
+
+    if (this.user) {
+      const cartResponse = await axios.get(`/api/users/${this.user.uid}/cart`);
       const cartItems = cartResponse.data;
       this.cartItems = cartItems;
     }
   }
+}
 </script>
